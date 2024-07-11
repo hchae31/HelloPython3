06a31d78cae4eebb778f1038defe10ab82a5d2b3
hchae31 <chaeheegon5858@gmail.com>
2024-07-11 16:20:49 +0900

    실습문제 추가

--- 03sungjukV1.py
+++ 03sungjukV1.py
@@ -11,9 +11,16 @@ average = total / 3
 # 학점 계산
 if average >= 90:
     grade = "수"
-if average >= 90:
-    grade = "수"
-if average >= 90:
-    grade = "수"
-if average >= 90:
-    grade = "수"
+elif average >= 80:
+    grade = "우"
+elif average >= 70:
+    grade = "미"
+elif average >= 60:
+    grade = "양"
+else:
+    grade = "가"
+
+# 3. 결과 출력
+print(f"이름 : {name}")
+print(f"국어 : {korean}, 영어: {english}, 수학: {math}")
+print(f"총점 : {total}, 평균: {average:.If}, 학점: {grade}")
