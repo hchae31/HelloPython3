683896aedabdfadc2cd01d5fd5709d44dd68109d
hchae31 <chaeheegon5858@gmail.com>
2024-07-23 17:09:02 +0900

    성적처리프로그램 v8 - 클래스 기반  : 추가/조회 기능 구현

---
+++ hchae31/oop/models.py
@@ -0,0 +1,13 @@
+class SungJuk:
+    # 생성자 : 클래스를 이용해서 객체를 만들떄 초기화 작업을 수행하는 특별한 함수
+    # 변수선언: self.변수명
+    def __init__(self, name, kor, eng, mat):
+        self.sjno = 0
+        self.name = '홍길동'
+        self.kor = 0
+        self.eng = 0
+        self.mat = 0
+        self.tot = 0
+        self.avg = 0.0
+        self.grd = '가'
+        self.regdate = '2024-07-23 17:00:00'
